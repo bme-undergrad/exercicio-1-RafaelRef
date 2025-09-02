d40dd1fcb73e90f0f357af54f3aeaca7ce5efa2f
Rafael Fernandez <rafayosano@.com>
2025-09-02 10:47:17 -0300

    mane

--- exercicio1.m
+++ exercicio1.m
@@ -1,29 +1,29 @@
 function t = exercicio1(func, func_d, x0)
 
 % nao alterar: inicio
-es = 0.001;
+es = 0.01;
 imax = 20;
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-t = zeros(20,1);
+t = zeros(imax,1);
 
-t(1)=0.1;
-erro = zeros (length(t),1);
+t(1)=x0;
+erro = zeros (imax,1);
 
 for k=1:imax
 
-    if k>=1:
+    if k=˜1
         t(k+1)=t(k)-func(t(k))/func_d(t(k));
-        erro(k)=abs((t(k)-t(k-1))/t(k))
+        erro(k)=abs((t(k)-t(k-1))/t(k));
 
-    if erro<es:
+    if erro<es
         break
 endfor
-resultadudo=t(find(t,1,'last'))
 
-t = 0; % alterar
+t = t(k); % alterar
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 endfunction
+
