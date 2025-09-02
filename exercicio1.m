bf623d7c235091b42d205bbd7b46167687292349
Rafael Fernandez <rafayosano@.com>
2025-09-02 13:52:35 -0300

    mane7

--- exercicio1.m
+++ exercicio1.m
@@ -1,4 +1,4 @@
-function t = exercicio1(func, func_d, x0)
+function t = exercicio1(func,func_d,x0)
 
 % nao alterar: inicio
 es = 0.01;
@@ -7,23 +7,19 @@ imax = 20;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-t = zeros(imax,1);
+t = zeros(imax, 1);
+t(1) = x0;
+erro = zeros(imax, 1);
 
-t(1)=x0;
-erro = zeros(imax,1);
-
-for ii = 1:length(ii)-1
-    if ii ~=1
-        t(ii+1)=t(ii)-func(t(ii))/func_d(t(ii));
-        erro(ii)=abs((t(ii)-t(ii-1))/t(ii));
-        if erro<es
-            break
-        endif
+for ii = 1:length(t)-1 %usar ii e n i pq i eh imaginarium
+  if ii ~= 1
+    erro(ii) = abs(t(ii) - t(ii-1))/t(ii); %abs é modulo
+    if erro(ii) < es
+      break
     endif
+  endif
+  t(ii+1) = t(ii) - func(t(ii))/func_d(t(ii)); %; faz sumir
 endfor
-
 t = t(ii); % alterar
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
-endfunction
-
